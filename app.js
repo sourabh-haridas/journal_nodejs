2e20ca1cc363c119576562dcb0bd998f50bfa08e
Sourabh <73120227+sourabh1500@users.noreply.github.com>
2022-12-28 14:53:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -109,6 +110,16 @@ app.get("/posts/:postId", function (req, res) {
   //   }
   // });
 });
+
+app.post('/delete/:postId', (req, res) => {
+  const requestedId = req.params.postId;
+
+  Post.deleteOne({_id: requestedId}, function(err) {
+    if(!err) {
+      res.redirect('/');
+    }
+  })
+})
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
